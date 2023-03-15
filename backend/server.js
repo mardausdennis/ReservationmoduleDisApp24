614c4d12aee81a391454ec29ca32de6db10d9f5d
mardausdennis <71312763+mardausdennis@users.noreply.github.com>
2023-03-15 18:41:31 +0100

    Add OAuth 2.0 authentication to server.js
    Implement Google OAuth 2.0 authentication flow in server.js. This includes generating an authorization URL, creating a callback route to handle the authentication code, and exchanging the code for an access token and refresh token. The refresh token is stored in the environment variables for later use.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,58 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const calendar = require('./routes/calendar');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+function getAuthUrl() {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/calendar'],
+  });
+  return authUrl;
+}
+
+app.get('/auth', (req, res) => {
+  const authUrl = getAuthUrl();
+  res.redirect(authUrl);
+});
+
+app.get('/auth/callback', async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    process.env.REFRESH_TOKEN = tokens.refresh_token;
+
+    res.send('Authentifizierung erfolgreich. Sie können jetzt die API verwenden.');
+  } catch (error) {
+    res.status(400).send('Authentifizierungsfehler: ' + error.message);
+  }
+});
+
+//eigenen Routen definieren
+app.use('/calendar', calendar);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
