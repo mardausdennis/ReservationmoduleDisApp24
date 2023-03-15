961a5b4cb36bfd69ffbea5635e0c1e9d365c22de
mardausdennis <71312763+mardausdennis@users.noreply.github.com>
2023-03-15 17:22:37 +0100

    Integrate required packages and configure .env file
    - Import necessary packages in app.js (Express, dotenv, googleapis, body-parser, cors)
    - Load environment variables from the .env file using dotenv.config()
    - Update .env file with the required Google Calendar API credentials and other configuration settings
    
    This commit sets up the basic structure and required packages for the backend server, as well as configures the .env file to securely store API credentials and other settings.

---
+++ backend/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const {google} = require('googleapis');
+const bodyParser = require('body-parser');
+const cors = require('cors');
